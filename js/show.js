978fad8c45496717592d2f8cc950396b0e72862d
aha <ntuaha@gmail.com>
2014-05-21 08:12:32 +0800

    修改bug

--- js/show.js
+++ js/show.js
@@ -40,7 +40,7 @@ var makeBar = function(width, height, bar_data) {
       .orient("left")
       .tickFormat(Y_DATA_FORMAT);
   
-  var value_data = _.map(groups, function(d) {
+  var value_data = bar_data.map(groups, function(d) {
     return {x_axis: d, y_axis: bar_data[d]};
   });
   
@@ -132,7 +132,7 @@ $(function(){
 
 
 
-    
+
     var keys = Object.keys(data[0]);
     for (var i = 0; i < keys.length; i++) {
       if (keys[i] !== "chart_title" && keys[i] !== "unit") {
